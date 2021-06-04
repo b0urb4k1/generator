f860e79b114baa21614efa4a9c47ce5c34675339
Nicolas Bourbaki <3071952+b0urb4k1@users.noreply.github.com>
2021-06-04 20:58:22 +0200

    task

--- build/tasks/Tasks.cs
+++ build/tasks/Tasks.cs
@@ -1,8 +1,30 @@
 ﻿using System;
+using Microsoft.Build.Framework;
 
 namespace build.tasks
 {
-    public class Class1
+    public class SimpleTask : ITask
     {
+        //When implementing the ITask interface, it is necessary to
+        //implement a BuildEngine property of type
+        //Microsoft.Build.Framework.IBuildEngine. This is done for
+        //you if you derive from the Task class.
+        public SimpleTask() {}
+
+        public IBuildEngine BuildEngine { get; set; }
+
+        // When implementing the ITask interface, it is necessary to
+        // implement a HostObject property of type object.
+        // This is done for you if you derive from the Task class.
+        ITaskHost ITask.HostObject { get; set; }
+
+        public string Parameter { get; set; }
+
+        public bool Execute()
+        {
+            Console.WriteLine($"Hello from Task {Parameter}.");
+
+            return true;
+        }
     }
 }
